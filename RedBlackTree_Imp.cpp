7f39a30b91a09fec3f833655d35e76ba56bdadca
DD898989 <killuplus300@gmail.com>
2019-02-02 15:46:49 +0800

    Update RedBlackTree_Imp.cpp

--- RedBlackTree_Imp.cpp
+++ RedBlackTree_Imp.cpp
@@ -1,78 +1,107 @@
-﻿//this solution comes from https://github.com/zyi23/Red-Black-Tree/blob/master/rbtree.cpp
-
+﻿//https://github.com/zyi23/Red-Black-Tree/blob/master/rbtree.cpp
 #include "stdafx.h"
 #include <iostream>
-#include <ctime>
 #include <iomanip>
 using namespace std;
-
-
-#define __TEST__
-
 template <class T>
 class rbtree;
-
+//----------------------------------------------------------
 template <class T>
 struct _rbTreeNode
 {
 	friend class rbtree<T>;
-	T getkey()
-	{
-		return key;
-	}
 
 private:
 	T key;
-	_rbTreeNode<T> *left;
-	_rbTreeNode<T> *right;
+	_rbTreeNode<T> *dir[2]; // 0:left  1:right
 	_rbTreeNode<T> *parent;
-	bool color;			// true == red  &&  false == black
+	bool color;	// true:red   false:black
 };
-
-
+//----------------------------------------------------------
 template <class T>
 class rbtree
 {
+private:
+	void Rotate(  _rbTreeNode<T> * ,bool bDir);//true:left rotate    false: right rotate
+	void rbDeleteFixup(  _rbTreeNode<T> * );
+	void rbInsertFixup( _rbTreeNode<T> * );
+	_rbTreeNode<T> *treeSuccessor(  _rbTreeNode<T> * );
+	_rbTreeNode<T> *nil;//null node
+
 public:
 	rbtree()
-		:nil( new _rbTreeNode<T> ), root( nil ) 
+		:nil( new _rbTreeNode<T> ), root( nil ) //root must below nil in class declaration
 	{ 
-		nil->left = 0; 
+		nil->dir[0] = 0; 
+		nil->dir[1] = 0; 
 		nil->parent = 0; 
-		nil->right = 0; 
 		nil->color = false; 
 	}
 
-	void insert( T key );
-	_rbTreeNode<T> * search( T key );
-	void erase( T key );
-
-#ifdef __TEST__
-	void display();
-#endif
-
-private:
-
-#ifdef __TEST__
-	void _display( _rbTreeNode<T> * );
-#endif
-
-
-	void leftRotate(  _rbTreeNode<T> * );
-	void rightRotate(  _rbTreeNode<T> * );
-
-	void rbDelete(  _rbTreeNode<T> * );
-	void rbDeleteFixup(  _rbTreeNode<T> * );
-
-	void rbInsertFixup( _rbTreeNode<T> * );
-
-	_rbTreeNode<T>* treeSuccessor(  _rbTreeNode<T> * );
-
-	_rbTreeNode<T> *nil;			//空结点
-	_rbTreeNode<T> *root;
-
+	void erase(T key);
+	void insert(T key);
+	void display( _rbTreeNode<T> * x );
+	_rbTreeNode<T> *root;//public for display
+	_rbTreeNode<T> *search( T key );
 };
+//----------------------------------------------------------
+template <class T>
+_rbTreeNode<T> * rbtree<T>::search( T key )
+{
+	_rbTreeNode<T> * x = root;
 
+	while( x != nil && key != x->key )
+			x = x->dir[(key > x->key)];
+
+	return x;
+}
+//----------------------------------------------------------
+template <class T>
+void rbtree<T>::erase( T key )
+{
+	_rbTreeNode<T> * z = search( key );
+
+	if( z != nil )
+	{
+		_rbTreeNode<T> * x = nil;
+		_rbTreeNode<T> * y = nil;
+
+		if( z->dir[0] == nil || z->dir[1] == nil )
+			y = z;
+		else
+			y = treeSuccessor( z );
+
+		if( y->dir[0] != nil )
+			x = y->dir[0];
+		else
+			x = y->dir[1];
+
+		x->parent = y->parent;
+
+		if( y->parent == nil )
+			root = x;
+		else
+		{
+			if( y == y->parent->dir[0] )
+				y->parent->dir[0] = x;
+			else
+				y->parent->dir[1] = x;
+		}
+
+		if( y != z )
+			z->key = y->key;
+		
+		cout<<"---------after erase--------"<<endl;   display(this->root);
+
+		if( y->color == false )
+			rbDeleteFixup( x );
+		
+		cout<<"---------after balance--------"<<endl;   display(this->root);
+
+		delete y;
+	}
+}
+//----------------------------------------------------------
 template <class T>
 void rbtree<T>::insert( T key )
 {
@@ -80,366 +109,192 @@ void rbtree<T>::insert( T key )
 	t->key = key;
 	_rbTreeNode<T>* x = root;
 	_rbTreeNode<T>* y = nil;
+
 	while( x != nil )
 	{
 		y = x;
-		if( key < x->key )
-			x = x->left;
-		else
-			x = x->right;
+		x = x->dir[(key > x->key)];
 	}
 
 	t->parent = y;
 	if( y == nil )
 		root = t;
 	else
-	{
-		if ( t->key < y->key )
-			y->left = t;
-		else
-			y->right = t;
-	}
+		y->dir[(t->key > y->key)] = t;
 
-	t->left = nil;
-	t->right = nil;
+	t->dir[0] = nil;
+	t->dir[1] = nil;
 	t->color = true;
 
+	cout<<"---------after insert--------"<<endl;   display(this->root);
 
-	cout<<"---------插入後--------"<<endl;   display();
-	
 	rbInsertFixup( t );
 
-	cout<<"---------排列後--------"<<endl;   display();
-	
+	cout<<"---------after balance--------"<<endl;   display(this->root);
 }
-
-template <class T>
-_rbTreeNode<T> * rbtree<T>::search( T key )
-{
-	_rbTreeNode<T> * x = root;
-	while( x != nil && key != x->key )
-	{
-		if( key < x->key )
-			x = x->left;
-		else
-			x = x->right;
-	}
-
-	return x;
-}
-
-template <class T>
-void rbtree<T>::erase( T key )
-{
-	_rbTreeNode<T> * x = search( key );
-	if( x != nil )
-		rbDelete( x );
-}
-
+//----------------------------------------------------------
 template <class T>
 _rbTreeNode<T> * rbtree<T>::treeSuccessor( _rbTreeNode<T> *x )
 {
-	if( x->right != nil )
+	if( x->dir[1] != nil )
 	{
-		while( x->left != nil )
-			x = x->left;
+		while( x->dir[0] != nil )
+			x = x->dir[0];
 		return x;
 	}
-
-	_rbTreeNode<T> * y = x->parent;
-	while( y != nil && x == y->right )
+	else
 	{
-		x = y;
-		y = y->parent;
+		_rbTreeNode<T> * y = x->parent;
+		while( y != nil && x == y->dir[1] )
+		{
+			x = y;
+			y = y->parent;
+		}
+		return y;
 	}
-
-	return y;
 }
-
+//----------------------------------------------------------
 template <class T>
-void rbtree<T>::leftRotate( _rbTreeNode<T> *x )  // x->right != nil
+void rbtree<T>::Rotate( _rbTreeNode<T> *x,bool bDir )  // x->dir[1] != nil
 {		
-	_rbTreeNode<T> * y = x->right;
+	_rbTreeNode<T> * y = x->dir[bDir];
+
 	if( x->parent == nil )
 		root = y;
 	else
-	{
-		if( x == x->parent->left )
-			x->parent->left = y;
-		else
-			x->parent->right = y;
-	}
+		x->parent->dir[(x != x->parent->dir[0])] = y;
 
 	y->parent = x->parent;
-	x->right = y->left;
-	y->left->parent = x;
-	y->left = x;
+	x->dir[bDir] = y->dir[!bDir];
+	y->dir[!bDir]->parent = x;
+	y->dir[!bDir] = x;
 	x->parent = y;
 }
-
-template <class T>
-void rbtree<T>::rightRotate( _rbTreeNode<T> *x )
-{
-	_rbTreeNode<T> * y = x->left;
-	if( x->parent == nil )
-		root = y;
-	else
-	{
-		if( x == x->parent->left )
-			x->parent->left = y;
-		else
-			x->parent->right = y;
-	}
-
-	y->parent = x->parent;
-	x->left = y->right;
-	y->right->parent = x;
-	y->right = x;
-	x->parent = y;
-}
-
+//----------------------------------------------------------
 template <class T>
 void rbtree<T>::rbInsertFixup( _rbTreeNode<T> *z )
 {
 	while( z->parent->color)
 	{
-		if( z->parent == z->parent->parent->left ) //if(){大括號內容根else相反的全都是方向}
-		{
-			_rbTreeNode<T> * y = z->parent->parent->right;
-			if( y->color)
-			{
-				z->parent->color = false;
-				y->color = false;
-				z->parent->parent->color = true;
-				z = z->parent->parent;
-			}
-			else
-			{
-				if( z == z->parent->right )
-				{
-					z = z->parent;
-					leftRotate( z );
-				}
-				z->parent->color = false;
-				z->parent->parent->color = true;
-				z->parent->right->color = false;
-				rightRotate( z->parent->parent );
-			}
+		bool bDir = (z->parent == z->parent->parent->dir[0]);
 
+		_rbTreeNode<T> * y = z->parent->parent->dir[bDir];
+		if( y->color)
+		{
+			z->parent->color = false;
+			y->color = false;
+			z->parent->parent->color = true;
+			z = z->parent->parent;
 		}
-		else									//else{大括號內容根if相反的全都是方向}
+		else
 		{
-			_rbTreeNode<T> * y = z->parent->parent->left;
-			if( y->color == true )
+			if( z == z->parent->dir[bDir] )
 			{
-				z->parent->color = false;
-				y->color = false;
-				z->parent->parent->color = true;
-				z = z->parent->parent;
+				z = z->parent;
+				Rotate( z,bDir );
 			}
-			else
-			{
-				if( z == z->parent->left )
-				{
-					z = z->parent;
-					rightRotate( z );
-				}
-
-				z->parent->color = false;
-				z->parent->parent->color = true;
-				z->parent->left->color = false;
-				leftRotate( z->parent->parent );
-			}
-
+			z->parent->color = false;
+			z->parent->parent->color = true;
+			z->parent->dir[bDir]->color = false;
+			Rotate( z->parent->parent,!bDir );
 		}
 
 	}
-
 	root->color = false;
 }
-
-template <class T>
-void rbtree<T>::rbDelete( _rbTreeNode<T> *z )
-{
-	_rbTreeNode<T> * x = nil;
-	_rbTreeNode<T> * y = nil;
-	if( z->left == nil || z->right == nil )
-		y = z;
-	else
-		y = treeSuccessor( z );
-	if( y->left != nil )
-		x = y->left;
-	else
-		x = y->right;
-	x->parent = y->parent;
-	if( y->parent == nil )
-		root = x;
-	else
-	{
-		if( y == y->parent->left )
-			y->parent->left = x;
-		else
-			y->parent->right = x;
-	}
-
-	if( y != z )
-		z->key = y->key;
-	if( y->color == false )
-		rbDeleteFixup( x );
-	delete y;
-}
-
+//----------------------------------------------------------
 template <class T>
 void rbtree<T>::rbDeleteFixup(_rbTreeNode<T> * x)
 {
 	while( x != root && x->color == false )
 	{
 		_rbTreeNode<T> * w = 0;
-		if( x->parent->left == x )
+		bool bDir = (x->parent->dir[0] == x);
+		w = x->parent->dir[bDir];
+
+		if(w->color)
 		{
-			w = x->parent->right;
-			if( w->color == true )
-			{
-				w->color = false;
-				x->parent->color = true;
-				leftRotate( x->parent );
-				w = x->parent->right;
-			}
-
-			if( w->left->color == false && w->right->color == false )
-			{
-				w->color = true;
-				x = x->parent;
-			}
-
-			else
-			{
-				if( w->right->color == false )
-				{
-					w->left->color = false;
-					w->color = true;
-					rightRotate( w );
-					w = x->parent->right;
-				}
-
-				w->color = x->parent->color;
-				x->parent->color = false;
-				w->right->color = false;
-				leftRotate( x->parent );
-				x = root;
-			}
-
+			w->color = false;
+			x->parent->color = true;
+			Rotate( x->parent,bDir );
+			w = x->parent->dir[bDir];
 		}
 
+		if( w->dir[!bDir]->color == false && w->dir[bDir]->color == false )
+		{
+			w->color = true;
+			x = x->parent;
+		}
 		else
 		{
-			w = x->parent->left;
-			if( w->color == true )
+			if( w->dir[bDir]->color == false )
 			{
-				w->color = false;
-				x->parent->color = true;
-				rightRotate( x->parent );
-				w = x->parent->left;
-			}
-
-			if( w->right->color == false && w->left->color == false )
-			{
+				w->dir[!bDir]->color = false;
 				w->color = true;
-				x = x->parent;
+				Rotate( w,!bDir );
+				w = x->parent->dir[bDir];
 			}
 
-			else
-			{
-				if( w->left->color == false )
-				{
-					w->right->color = false;
-					w->color = true;
-					leftRotate( w );
-					w = x->parent->left;
-				}
-
-				w->color = x->parent->color;
-				x->parent->color = false;
-				w->left->color = false;
-				rightRotate( x->parent );
-				x = root;
-			}
-
+			w->color = x->parent->color;
+			x->parent->color = false;
+			w->dir[bDir]->color = false;
+			Rotate( x->parent,bDir );
+			x = root;
 		}
-
 	}
 
 	x->color = false;
 }
-
-
-#ifdef __TEST__
-
+//----------------------------------------------------------
 template <class T>
-void rbtree<T>::display()
+void rbtree<T>::display( _rbTreeNode<T> * x )
 {
-	if( root != nil )
-		_display( root );
+	if( root == nil )
+		cout << "Tree is empty!" << endl;
 	else
-		cout << "Tree is empty! " << endl;
-}
-
-
-template <class T>
-void rbtree<T>::_display( _rbTreeNode<T> * x )
-{
-	if( x->left != nil )
-		_display( x->left );
-	if( x != nil )
 	{
-		int wid=10;
+		if( x->dir[0] != nil )
+			display( x->dir[0] );
 
-		
-		if( x->parent != nil )
-			cout << setw(wid) << ' '<< x->parent->key;
-		else
-			cout << setw(wid)<< "  　";
+		if( x != nil )
+		{
+			int wid=10;
 
-		cout<<endl;//-----------------------------------------
+			if( x->parent != nil )
+				cout << setw(wid) << ' '<< x->parent->key;
+			else
+				cout << setw(wid)<< "  　";
 
-		cout<< setw(wid) <<"【"<< x->key << "】";
+			cout<<endl;//-----------------------------------------
 
+			cout<< setw(wid) <<"【"<< x->key << "】";
 
-		if( x->color == true )
-			cout << "R";
-		else
-			cout << "B";
+			if( x->color)
+				cout << "R";
+			else
+				cout << "B";
 
-		
-		cout<<endl;//-----------------------------------------
+			cout<<endl;//-----------------------------------------
 
+			if( x->dir[0] != nil )
+				cout << setw(wid/2)<< x->dir[0]->key << ' ';
+			else
+				cout << setw(wid/2)<< " 　";
 
-		if( x->left != nil )
-			cout << setw(wid/2)<< x->left->key << ' ';
-		else
-			cout << setw(wid/2)<< " 　";
+			if( x->dir[1] != nil )
+				cout << setw(wid)<< x->dir[1]->key << ' ';
+			else
+				cout << setw(1.5*wid)<< "　";
 
-		
+			cout<<endl;//-----------------------------------------
+		}
 
-
-		if( x->right != nil )
-			cout << setw(wid)<< x->right->key << ' ';
-		else
-			cout << setw(1.5*wid)<< "　";
-
-
-		
-		cout<<endl;//-----------------------------------------
+		cout << endl;
+		if( x->dir[1] != nil )
+			display( x->dir[1] );
 	}
-
-	cout << endl;
-	if( x->right != nil )
-		_display( x->right );
 }
-
-#endif
-
-
+//----------------------------------------------------------
 int main()
 {
 	rbtree<int> test;
@@ -451,18 +306,13 @@ int main()
 	test.insert(3);
 	test.insert(2);
 
-	
-	int a;
-	test.display();
-	cout<<"開始移除:"<<endl;;
-	while( cin >> a )
-	{
-		test.erase( a );
-		test.display();
-		cout<<"開始移除:"<<endl;;
-	}
-
+	test.erase(6);
+	test.erase(5);
+	test.erase(4);
+	test.erase(2);
+	test.erase(3);
 
 	system("pause");
 	return 0;
 }
+//----------------------------------------------------------
